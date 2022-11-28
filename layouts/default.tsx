d2a4d5644b5b4b66f51863e99e9f98ac36937992
dongsun <le12352@gractor.com>
2022-11-28 17:13:20 +0900

    add eslint, prettier

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,9 +1,10 @@
-import Header from "../components/header";
+import Header from '../components/header';
 
-export default function Home() {
+export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
+      {children}
     </>
   );
 }
