0fc4d90a92b9a51e84a5fa315d58aa227d72bbe0
dongsun <le12352@naver.com>
2022-12-06 09:05:47 +0900

    add axios

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,9 @@
 import Axios from 'axios';
 
+const dev = process.env.NODE_ENV !== 'production';
+
 const axios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API,
+  baseURL: dev ? 'http://localhost:3000' : 'https://dongsun1-dev.vercel.app',
 });
 
 export default axios;
