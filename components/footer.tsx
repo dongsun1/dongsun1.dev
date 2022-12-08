adf33426dfa80ce392a5715266ae0f9f12ace0ac
dongsun1 <le12352@naver.com>
2022-12-08 20:54:18 +0900

    change linkedin image

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,12 +2,11 @@ import Link from 'next/link';
 import Github from '../public/github.png';
 import GithubWhite from '../public/github-white.png';
 import LinkedIn from '../public/linkedin.png';
-import LinkedInWhite from '../public/linkedin-white.png';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
 
 export default function Footer() {
-  const { theme } = useTheme();
+  const { theme = 'dark' } = useTheme();
 
   return (
     <footer className="container text-gray-600 body-font mx-auto mt-auto w-full">
@@ -16,8 +15,8 @@ export default function Footer() {
           <span className="ml-3 text-xl dark:text-gray-300">dongsun1 blog</span>
         </a>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <Link href="https://www.linkedin.com/in/%EB%8F%99%EC%84%A0-%EA%B9%80-5197ba219/" className="ml-3 flex items-centers dark:bg-white">
-            <Image src={theme === 'dark' ? LinkedInWhite : LinkedIn} alt="링크드인" width={20} height={20} />
+          <Link href="https://www.linkedin.com/in/%EB%8F%99%EC%84%A0-%EA%B9%80-5197ba219/" className="ml-3 flex items-centers bg-parent">
+            <Image src={LinkedIn} alt="링크드인" width={20} height={20} />
           </Link>
           <Link href="https://www.github.com/dongsun1" className="ml-3 flex items-centers">
             <Image src={theme === 'dark' ? GithubWhite : Github} alt="깃허브" width={20} height={20} />
