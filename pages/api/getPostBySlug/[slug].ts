07169fd16a92ff116f98375bf9ac5a66bfa9d152
dongsun <le12352@naver.com>
2022-12-14 13:07:10 +0900

    update getPost logic

--- pages/api/getPostBySlug/[slug].ts
+++ pages/api/getPostBySlug/[slug].ts
@@ -1,10 +1,35 @@
+import path from 'path';
+import fs from 'fs';
+import matter from 'gray-matter';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getAllPosts } from '../getAllPosts';
+import { IPost } from '../../../interfaces/post.interface';
+
+const recursiveGetPost = ({ filePath, slug }: { filePath: string; slug: string }) => {
+  const files = fs.readdirSync(filePath);
+
+  const posts = files.reduce<IPost[]>((acc, fileName) => {
+    if (!fileName.includes('.md')) {
+      acc.push(...recursiveGetPost({ filePath: `${filePath}/${fileName}`, slug }));
+    } else if (fileName.replace('.md', '') === slug) {
+      const file = fs.readFileSync(`${filePath}/${fileName}`, 'utf-8');
+      const { data: frontMatter, content } = matter(file);
+
+      acc.push({
+        frontMatter,
+        slug: fileName.replace(/\.md/, ''),
+        content,
+      });
+    }
+    return acc;
+  }, []);
+
+  return posts;
+};
 
 export async function getPostBySlug({ slug }: { slug: string }) {
-  const posts = await getAllPosts();
+  const postsDirectory = path.join(process.cwd(), 'public');
+  const [post] = recursiveGetPost({ filePath: `${postsDirectory}/posts`, slug });
 
-  const post = posts.find(({ slug: _slug }) => _slug === slug);
   if (post) {
     return post;
   }
