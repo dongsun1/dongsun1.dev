b76e40953e09d79e348d28667cdecb0028dfb45a
dongsun <le12352@naver.com>
2023-01-10 11:11:53 +0900

    index delete useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,30 +4,26 @@ import Posts from '../components/posts';
 import Sidebar from '../components/sidebar';
 import Pagination from '../components/pagination';
 import { ICategoryCounts, IPost } from '../interfaces/post.interface';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Container from '../components/container';
 import axios from '../lib/axios';
 
 export default function Index({ posts, categoryCounts, total }: { posts: IPost[]; categoryCounts: ICategoryCounts; total: number }) {
   const [page, setPage] = useState(1);
   const [count, setCount] = useState(Math.ceil(total / 5));
-  const [filteredPosts, setFilteredPosts] = useState(posts);
   const [paginationPosts, setPaginationPosts] = useState(posts);
 
   const getPosts = async ({ category }: { category: string }) => {
     const { data: { posts, total } = {} } = await axios.get('/api/getPaginationPosts', {
       params: { category },
     });
+    setPage(1);
     setCount(Math.ceil(total / 5));
-    setFilteredPosts(posts);
+    setPaginationPosts(posts);
   };
 
-  useEffect(() => {
-    setPage(1);
-    setPaginationPosts(filteredPosts);
-  }, [filteredPosts]);
-
   const paging = async (e: any, page: number) => {
+    console.info('paging');
     setPage(page);
 
     const { data: { posts } = {} } = await axios.get('/api/getPaginationPosts', {
