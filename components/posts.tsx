0ea4d05a53a957e81389f9e724ea4ca31655f646
dongsun <le12352@naver.com>
2023-01-10 16:07:24 +0900

    add NoSsr

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,3 +1,4 @@
+import { NoSsr } from '@mui/material';
 import Link from 'next/link';
 import { IPost } from '../interfaces/post.interface';
 
@@ -14,7 +15,9 @@ export default function Posts({ posts }: { posts: IPost[] }) {
               <div className="py-8 flex flex-wrap md:flex-nowrap">
                 <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                   <div className="font-semibold title-font text-gray-700 dark:text-gray-300">{category}</div>
-                  <div className="mt-1 text-gray-500 text-sm dark:text-gray-400">{formattedDate}</div>
+                  <NoSsr>
+                    <div className="mt-1 text-gray-500 text-sm dark:text-gray-400">{formattedDate}</div>
+                  </NoSsr>
                 </div>
                 <div className="flex flex-col md:flex-grow">
                   <Link href={`/${_id}`} className="text-2xl font-medium text-gray-900 title-font mb-2 dark:text-gray-300">
