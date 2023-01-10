07f476eca849badd62a241f1e78b5db0b4e8e554
dongsun <le12352@naver.com>
2023-01-10 10:59:59 +0900

    delete favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="google-site-verification" content="w7Y4Ecdgxm4HMl5mivESvs1fQVUricgDzI_ugwuPBu0" />
-          <link href="/favicon.ico" rel="shortcut icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"></link>
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR&display=swap" rel="stylesheet" />
